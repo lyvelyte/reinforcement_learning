07bc1eb5dda490791ece56928cb5af6a1b276a0b
Brendan Alvey <bja3md@umsystem.edu>
2023-10-07 13:38:23 -0500

    Working lstm cnn for breakout/carpole.

--- marioppo/alveybj/Test.py
+++ marioppo/alveybj/Test.py
@@ -1,24 +1,22 @@
-import gym
+import gymnasium as gym
 from sb3_contrib import RecurrentPPO
-from nes_py.wrappers import JoypadSpace
-import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from stable_baselines3 import PPO
-from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv, NoopResetEnv
+from stable_baselines3.common.atari_wrappers import AtariWrapper
 
-# model = RecurrentPPO.load("tmp/best_model.zip")
-model = PPO.load("tmp/best_model.zip")
+# model = RecurrentPPO.load("ppo_breakout_model_cnnlstmppo.zip")
+model = PPO.load("ppo_breakout_model_cnnppo.zip")
 
-def make_env():
-    env = gym.make("ALE/Breakout-v5", render_mode='human', max_episode_steps=10000)
-    env = NoopResetEnv(env, noop_max=30)
-    env = MaxAndSkipEnv(env, skip=4)
+def make_env(env_id):
+    env = gym.make(env_id, render_mode='human')
+    env = AtariWrapper(env, noop_max=30, frame_skip=4, screen_size=84, terminal_on_life_loss=True, clip_reward=True, action_repeat_probability=0.0)
+    env.reset()
     return env
 
 def main():
-    # env = gym.make('SuperMarioBrosRandomStages-v0', stages=['1-1'], render_mode="human", apply_api_compatibility=True)
-    env =  make_env()
-    env.reset()
+    # env_id = "ALE/Breakout-v5"
+    env_id = "CartPole-v1"
+    
+    env =  make_env(env_id)
     obs, rew, done, trunc, info = env.step(env.action_space.sample())
     done = False
     while not done:
--- marioppo/alveybj/Train.py
+++ marioppo/alveybj/Train.py
@@ -1,95 +1,21 @@
-import retro
 import gymnasium as gym
-# import gym as old_gym
-from gymnasium.wrappers import StepAPICompatibility
-import os
-from stable_baselines3.common import results_plotter
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecMonitor
-from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv, NoopResetEnv
-from stable_baselines3.common.env_checker import check_env
-from gymnasium.wrappers import RecordEpisodeStatistics, AtariPreprocessing
-
-import numpy as np
-
 from sb3_contrib import RecurrentPPO
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.atari_wrappers import AtariWrapper
 
-import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-  
-
-class SaveOnBestTrainingRewardCallback(BaseCallback):
-    """Callback to save the best model based on training reward.
-
-    The check is performed every `check_freq` steps based on the training reward.
-    It is recommended to use `EvalCallback` for practical usage.
-
-    Parameters:
-        check_freq (int): Frequency of checks.
-        log_dir (str): Directory where the model and log will be saved.
-        verbose (int, optional): Verbosity level. Defaults to 1.
-    """
-    
-    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
-        super().__init__(verbose)
-        self.check_freq = check_freq
-        self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, 'best_model')
-        self.best_mean_reward = -np.inf
-        self.reward_buffer = np.zeros((10000, 1))
-        self.step_cnt = 0
-
-    def _init_callback(self) -> None:
-        """Create directory for saving models if it does not exist."""
-        os.makedirs(self.save_path, exist_ok=True)
-
-    def _on_step(self) -> bool:
-        """Save the best model based on the mean reward of the last 100 episodes."""
-        self.reward_buffer[self.step_cnt % len(self.reward_buffer)] = np.sum(self.locals.get('rewards', None))
-        self.step_cnt = self.step_cnt + 1
-
-        if self.n_calls % self.check_freq == 0:
-            if np.mean(self.reward_buffer) > 0:
-                mean_reward = np.mean(self.reward_buffer)
-                if self.verbose > 0:
-                    print(f"Num timesteps: {self.num_timesteps}")
-                    print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
-
-                if mean_reward > self.best_mean_reward:
-                    self.best_mean_reward = mean_reward
-                    if self.verbose > 0:
-                        print(f"Saving new best model to {self.save_path}")
-                    self.model.save(self.save_path)
-
-        return True
-
-
-def make_env():
-    env = gym.make("ALE/Breakout-v5", render_mode='rgb_array', max_episode_steps=10000)
-    env = NoopResetEnv(env, noop_max=30)
-    env = MaxAndSkipEnv(env, skip=4)
-    env = RecordEpisodeStatistics(env)
-    return env
-
-
-def main():
-    log_dir = "tmp/"
-    os.makedirs(log_dir, exist_ok=True)
-    # Instantiate the agent with a PPO with LSTM policy
-    env = make_env() 
-    # model = RecurrentPPO("CnnLstmPolicy", env, verbose=1, device="cuda", tensorboard_log="./board/", learning_rate=3e-5, ent_coef=1e-2)
-    model = PPO('CnnPolicy', env, verbose=1, device="cuda", tensorboard_log="./board/", learning_rate=3e-5, ent_coef=1e-2)
-    
-    # Train the agent
-    print("Training mario LSTM agent...")
-    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
-    model.learn(total_timesteps=2e6, callback = callback, tb_log_name="PPO-LSTM-00001")
-
-    # Save the trained model
-    model.save("ppo_breakout")
+def make_env(env_id: str, render_mode: str):
+    env = gym.make(env_id, render_mode=render_mode)
+    env.reset()
+    env = AtariWrapper(env, noop_max=30, frame_skip=4, screen_size=84, terminal_on_life_loss=True, clip_reward=True, action_repeat_probability=0.0)
+    return env 
 
 if __name__ == "__main__":
-    main()
+    env_id = "ALE/Breakout-v5"
+    # env_id = "CartPole-v1"
+    num_cpu = 18
+    vec_env = SubprocVecEnv([make_env(env_id, i, "rgb_array") for i in range(num_cpu)])
+    # model = PPO("CnnPolicy", vec_env, verbose=1, tensorboard_log="./board/", learning_rate=3e-5, ent_coef=1e-2)
+    model = RecurrentPPO("CnnLstmPolicy", vec_env, verbose=1, tensorboard_log="./board/", learning_rate=3e-5, ent_coef=1e-2)
+    model.learn(total_timesteps=2e5)
+    model.save("ppo_breakout_model")
